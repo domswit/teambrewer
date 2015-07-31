e698171fffe96f9bb1cf203298b235470ae53b71
Aron Santos <ajtsantos30@gmail.com>
2015-07-31 10:16:29 +0800

    aron commit

--- sched.js
+++ sched.js
@@ -8,7 +8,7 @@ angular.module('myApp', ['ngCookies']).controller('userCtrl', function($scope,
   $scope.updateData = {}
   $scope.first_name2 = "test";
 $scope.pageArray = [];
-
+var access_token = $cookies.get('access_token');
 
 $scope.fillPageArray = function(num, page) {
 
@@ -55,7 +55,7 @@ $scope.fillPageArray = function(num, page) {
 
       "http://localhost/teambrewer/API/sched-list.php?rand=" + new Date()
       .getTime() + "&page=" + page, {
-        'access_token': $cookies.get('access_token')
+        'access_token': access_token
       });
 
     response.success(function(data, status, headers, config) {
